bdfa4af74fb71afa2b5956697674a8fe1f4f2233
jacoboArcila <wagner.arcila.jacobo@gmail.com>
2023-02-03 16:51:52 -0500

    extender interfaces

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,13 +1,15 @@
-import {addProduct} from './products/product.service';
+import { addProduct } from './products/product.service';
 
 addProduct({
   id: '1',
   title: 'p1',
   createdAt: new Date(),
+  updateAt: new Date(),
   stock: 90,
   category: {
     id: '12',
-    name: 'a'
-  }
-})
-
+    name: 'a',
+    createdAt: new Date(),
+    updateAt: new Date(),
+  },
+});
