3a8cf94068b562d1049847637c723e62f776154e
jacoboArcila <wagner.arcila.jacobo@gmail.com>
2023-02-03 12:30:23 -0500

    parametros por defecto typescript

---
+++ src/06-default.ts
@@ -0,0 +1,27 @@
+export const createProduct = (
+  id: string | number,
+  //esto hace que sea opcional
+  isNew: boolean = true,
+  stock: number = 10,
+) => {
+  return {
+    id,
+    isNew: isNew,
+    stock: stock,
+  }
+}
+
+/*
+  0 === false
+  '' === false
+  false === false
+*/
+
+const p1 = createProduct(1, true, 12);
+console.log(p1);;
+
+const p2 = createProduct(1);
+console.log(p2);
+
+const p3 = createProduct(1, false, 0);
+console.log(p3);
